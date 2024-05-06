f25a18a2aa325c8756b1637fef1382d1e2919db5
Subash33 <subash.khanal.cs@gmail.com>
2024-05-06 17:20:02 -0500

    device flag updated

--- SoundBind/train.py
+++ SoundBind/train.py
@@ -28,7 +28,7 @@ if __name__=='__main__':
 
     parser = ArgumentParser(description='')
     parser.add_argument('--wandb_mode', type=str, default='disabled')
-    parser.add_argument('--devices', type=int, default=1)
+    parser.add_argument('--devices', type=str, default="auto")
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--num_workers', type=int, default=16)
     parser.add_argument('--run_name', type=str, default='test')
